e51a53b86d4b7a6fe8b70b7ed67659a2d3a60868
UWETom123 <tom.moreton@yahoo.co.uk>
2016-09-28 22:13:17 +0100

    Controllers now supported + jumping working

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,61 +4,71 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
     public float playerSpeed;
-    public float jumpStrength;
-
-    public int playerID;
 
     public string jumpButton = "Jump_P1";
     public string horizontalCtrl = "Horizontal_P1";
 
+    public int jumpPower;
+
     public float horizontal;
+    public float jump;
 
-	// Use this for initialization
-	void Start () {
+    public bool grounded;
 
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    Rigidbody2D rb2d;
+    RaycastHit2D hit;
 
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        grounded = true;
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        grounded = false;
+    }
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        jumpPower = 300;
+
+        if (rb2d.velocity.y > 0)
+        {
+            grounded = false;
+        }
         playerSpeed = 5.0f;
 
         horizontal = Input.GetAxis(horizontalCtrl);
 
-        if (Input.GetButton(horizontalCtrl))
-        {
-            if (horizontal == -1)
-            {
-                transform.position += Vector3.left * playerSpeed * Time.deltaTime;
-            }
-            else
-            {
-                transform.position += Vector3.right * playerSpeed * Time.deltaTime;
-            }
+        jump = Input.GetAxis(jumpButton);
 
+        if (horizontal == -1)
+        {
+            transform.position += Vector3.left * playerSpeed * Time.deltaTime;
         }
 
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    transform.position += Vector3.right * playerSpeed * Time.deltaTime;
-
-        //}
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    transform.position += Vector3.up * playerSpeed * Time.deltaTime;
-
-        //}
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    transform.position += Vector3.down * playerSpeed * Time.deltaTime;
-
-        //}
-
-        if (Input.GetButtonDown(jumpButton))
+        else if (horizontal == 1)
         {
+            transform.position += Vector3.right * playerSpeed * Time.deltaTime;
+        }
 
+        else if (jump == 1 && grounded == true)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower);
         }
 
     }
+
 }
