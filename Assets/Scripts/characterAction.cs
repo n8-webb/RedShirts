043317b37b4291f9d8e1a27a8cd5b76873bd2ea6
rc3-johnson <ryan@ryan-j.co.uk>
2016-09-28 14:32:07 +0100

    Updated characterAction script to add functions and variables

--- Assets/Scripts/characterAction.cs
+++ Assets/Scripts/characterAction.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class characterAction : MonoBehaviour {
 
+    public bool isAlive;
+    public bool canDie;
+    public bool crouching;
+    public bool isReloading;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +17,34 @@ public class characterAction : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    // Called when spawning a player
+    void playerSpawn()
+    {
+
+    }
+
+    // Called when a player dies
+    void playerDie()
+    {
+
+    }
+
+    // Called when a player starts crouching
+    void playerCrouch()
+    {
+        crouching = true;
+    }
+
+    //Called when a player stops crouching
+    void playerStand()
+    {
+        crouching = false;
+    }
+
+    // Called when a player is reloading
+    void playerReload()
+    {
+
+    }
 }
