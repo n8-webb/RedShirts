9c60e9dff3f33b9808768ee724424432d226a320
UWETom123 <tom.moreton@yahoo.co.uk>
2016-10-04 16:23:14 +0100

    Now have to press 'start' on controller in order for player to be enabled

---
+++ Assets/Scripts/Player/activatePlayer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activatePlayer : MonoBehaviour {
+
+    public string activateButton = "Activate_P1";
+    public string playerToActivate = "testPlayer_P1";
+    public bool activated;
+    public float activateValue;
+    GameObject player;
+
+
+    // Use this for initialization
+    void Start () {
+
+        player = GameObject.Find(playerToActivate);
+        player.SetActive(false);
+        activated = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        activateValue = Input.GetAxis(activateButton);
+
+        if (activateValue == 1 && activated == false)
+        {
+            player.SetActive(true);
+            activated = true;
+
+
+        }
+
+    }
+}
