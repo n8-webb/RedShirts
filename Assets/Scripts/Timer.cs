daa3fafc1425a81e01727540b2170141418f48f2
rc3-johnson <ryan@ryan-j.co.uk>
2016-10-05 08:48:57 +0100

    Score screen after timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -25,7 +25,11 @@ public class Timer : MonoBehaviour
             timeText.text = timeLeft.ToString("F2"); //Formats the timer to 00:00
             //RoundEnd();
         }
+        else
+        {
+            timeLeft = 0;
 
+        }
     }
 
     void RoundEnd() //Called when the timer reaches 0
